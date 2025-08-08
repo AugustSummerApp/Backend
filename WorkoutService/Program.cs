6b82f95c5b2543956c56d01da3bc0d56e7cebcd4
Augustvilliam <willyp@hotmail.se>
2025-08-08 16:42:30 +0200

    Added missing cors

--- WorkoutService/Program.cs
+++ WorkoutService/Program.cs
@@ -6,6 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WorkoutDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        policy => policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
@@ -18,6 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowReact");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
